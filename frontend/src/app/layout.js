497fa1ed2d200a94722223b0b3a58d3870f2aa9b
Steven <stevenpluas.22@hotmail.com>
2025-07-18 08:03:54 -0500

    Correcciones

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,3 @@
-// src/app/layout.js
 import './globals.css'
 
 import LayoutWithHeader from './Components/LayoutWithHeader'
@@ -11,15 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-<<<<<<< HEAD
-      <body className="bg-gray-50 text-gray-900 font-sans flex flex-col min-h-screen overflow-x-hidden">
-        <Header />
-        <main className="flex-grow mt-1">{children}</main>
-        <Footer />
-=======
       <body className="overflow-x-hidden bg-gray-50 text-gray-900 font-sans flex flex-col min-h-screen">
         <LayoutWithHeader>{children}</LayoutWithHeader>
->>>>>>> b88896e4b1d9a26072ddd5e0cfa18a8fdf50f4b4
       </body>
     </html>
   )
